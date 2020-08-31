f800a179136e1848c99cbf5c94074fd64c62c487
Muneera-Salah <muneera.salah@gmail.com>
2020-08-31 18:58:31 +0300

    add index route and manage.py file

--- app.py
+++ app.py
@@ -6,8 +6,19 @@ from flask_cors import CORS
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
-  CORS(app)
 
+  # CORS(app)
+  cors = CORS(app, resources={r"*": {"origins": "*"}})
+  # CORS Headers 
+  @app.after_request
+  def after_request(response):
+      response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+      response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+      return response
+
+  @app.route('/', methods=['GET'])
+  def index():
+    return 'index'
   return app
 
 APP = create_app()
